38d5b6595a29a48586ae1c85f82d6a127c1f0e0b
Juan Cruz <jc23729@gmail.com>
2021-02-14 16:37:36 -0500

    file setup and imports

---
+++ app.js
@@ -0,0 +1,8 @@
+//Imports 
+//itemRoutes is coming from folder routes, items.js file
+
+const express = require('express');
+const app = express();
+const itemRoutes = require('./routes/items')
+const ExpressError = require('expressError');
+
