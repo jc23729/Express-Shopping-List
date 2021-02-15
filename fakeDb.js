022f4a98478dc51af61734dfbde821e000e5d712
Juan Cruz <jc23729@gmail.com>
2021-02-14 19:23:28 -0500

    item.js

---
+++ fakeDb.js
@@ -0,0 +1,10 @@
+// Remember that since you are using an array for storage, this will be cleared each time the server restarts. Create a simple file called fakeDb.js which contains the following:
+
+// global.items = []
+
+// module.exports = items
+
+
+global.items = []
+
+module.exports = items
