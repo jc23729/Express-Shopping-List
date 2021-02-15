25e105f61210fe02b60d46c5c8788597ed3e9477
Juan Cruz <jc23729@gmail.com>
2021-02-14 19:32:22 -0500

    items.test.js

--- item.js
+++ item.js
@@ -32,14 +32,57 @@
 /**Item in shopping cart */
 const items = require('./fakeDb')
 
+
+// The constructor() method is called automatically when a class is initiated, 
+// and it has to have the exact name "constructor", in fact, if you do not have a constructor method, 
+// JavaScript will add an invisible and empty constructor method.
+
+/** Update found item with matching name to data. */
 class Item {
     constructor(name, price) {
-        this.name = name;
-        this.price = price;
-        //keep track of all items
-        items.push(this);
+      this.name = name;
+      this.price = price;
+  
+      // keep track of all items
+      items.push(this);
     }
-    static findAll({
-        return items
-    })
-}
+  
+    static findAll(){
+      return items
+    }
+    /** Update found item with matching name to data. */
+  
+    static update(name, data) {
+      let foundItem = Item.find(name);
+      if (foundItem === undefined) {
+        throw {message: "Not Found", status: 404}
+      }
+      foundItem.name = data.name;
+      foundItem.price = data.price;
+  
+      return foundItem;
+    }
+  
+    /** Find & return item with matching name. */
+  
+    static find(name) {
+      const foundItem = items.find(v => v.name === name);
+      if(foundItem === undefined){
+        throw { message: "Not Found", status: 404 }
+      }
+      return foundItem
+    }
+  
+    /** Remove item with matching id. */
+  
+  
+    static remove(name) {
+      let foundIdx = items.findIndex(v => v.name === name);
+      if (foundIdx === -1) {
+        throw {message: "Not Found", status: 404}
+      }
+      items.splice(foundIdx, 1);
+    }
+  }
+  
+  module.exports = Item;
